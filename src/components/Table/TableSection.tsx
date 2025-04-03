a73d4ea2d2246b5591a2ab94471b185bb577b7d3
jcdev92 <jcdeveloper92@gmail.com>
2025-04-03 19:10:35 -0400

    elimiando console.log ya no necesarios

--- src/components/Table/TableSection.tsx
+++ src/components/Table/TableSection.tsx
@@ -5,8 +5,6 @@ import { useSimulationForm } from '../../hooks/useSimulationForm';
 export const TableSection = () => {
   const simulationForm = useSimulationForm();
   const calculatedRows = useCalculations(simulationForm as SimulationForm);
-  console.log("calculatedRows", calculatedRows);
-  console.log("simulationForm", simulationForm);
 
   const dataCols = [
     "Mes",
