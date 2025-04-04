7c172e2fc9b0e6370f83f977fb4010e34357d425
jcdev92 <jcdeveloper92@gmail.com>
2025-04-04 12:10:29 -0400

    style(estilos): :lipstick: titulo - estilo cambiado

--- src/components/CommissionSimulator.tsx
+++ src/components/CommissionSimulator.tsx
@@ -4,7 +4,7 @@ export const CommissionSimulator = () => {
 
     return (
         <div className='w-full h-fit md:min-h-screen flex flex-col items-center justify-evenly gap-5'>
-            <h1 className='p-5 text-center text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-600 drop-shadow-lg'>
+            <h1 className='p-5 text-center text-4xl font-extrabold text-white drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)]'>
                 Simulador de Comisiones
             </h1>
             <FormSection />
